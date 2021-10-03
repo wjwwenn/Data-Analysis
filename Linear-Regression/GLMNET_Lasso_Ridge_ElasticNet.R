d1f02d155e0892ed6df3f678c2efc957827bffde
Jingwen <69618729+wjwwen@users.noreply.github.com>
2021-10-03 21:21:52 +0800

    Add files via upload

---
+++ Linear-Regression/GLMNET_Lasso_Ridge_ElasticNet.R
@@ -0,0 +1,74 @@
+# Best OLS defined by minimizing the sum of squared residuals
+# E(MSE) = sum of Variances, Squared bias, Irreducible errors
+
+# GLOSSARY
+# Glmnet: fits generalized linear and similar models via penalized maximum likelihood. 
+# cv.glmnet: main function to do cross-validation
+# alpha: elastic net mixing parameter, 𝛼∈[0,1]
+# a = 1: lasso, a=0: ridge
+
+# LASSO - Least Absolute Shrinkage and Selection Operator
+# Shrinks the regression coefficients toward 0 by penalizing the regression model
+# with a penalty term called L1-norm, which is the sum of the absolute coefficients
+# RIDGE
+# Shrinks the regression coefficients, so that variables, 
+# with minor contribution to the outcome, have their coefficients close to zero
+# with a penalty term called L2-norm, which is the sum of squared coefficients
+# Amt of penalty fine-tuned using constant called lambda.
+# ELASTIC NET
+# penalized with both L1-norm and L2-norm
+# shrink coefficients (like ridge) and set coefficients to zero (like lasso)
+# a hyperparameter “alpha” is provided to assign how much weight 
+# is given to each of the L1 and L2 penalties.
+
+install.packages("fastDummies")
+
+# load data: 
+library(glmnet)
+
+# used latin1 instead of UTF-8 encoding to load, if not file does not work
+sample1=read.csv("/Users/jingwen/Desktop/AN6005 Foundation of Statistical Analysis/PPT's and data files/Week 10-Regression analysis 1/Week 10 mini-lecture files/week10_credit.csv", header=TRUE, stringsAsFactors=FALSE, fileEncoding="latin1")
+
+# create dummy variables for categorical variables
+library(fastDummies)
+x=dummy_cols(sample1[,c("Gender","Student","Married",
+                        "Ethnicity")], remove_first_dummy = TRUE)
+y=sample1[,"Balance"]
+
+# removing categorical variables
+x=subset(x,select = -c(Gender))
+x=subset(x,select = -c(Student))
+x=subset(x,select = -c(Married))
+x=subset(x,select = -c(Ethnicity))
+
+# cv.glmnet can fit ridge and LASSO regression, by selection the “alpha” value
+# alpha = 1, 0; cv.glmnet is a general method (elastic net method)
+lasso=cv.glmnet(as.matrix(x),y,alpha=1)
+ridge=cv.glmnet(as.matrix(x),y,alpha=0)
+cv1 <- lasso
+cv0 <- ridge
+
+# Extract the coefficients from lasso$glmnet.fit$beta, 
+# based on the lambda value that gives the lowest MSE in the cv test (==lasso$lambda.min)
+lasso$glmnet.fit$beta[,lasso$glmnet.fit$lambda==lasso$lambda.min]
+ridge$glmnet.fit$beta[,ridge$glmnet.fit$lambda==ridge$lambda.min]
+
+# (3) Plot and compare with $cvm and $lambda
+# Cramér–von Mises criterion is a criterion used for judging the goodness of fit 
+# of a cumulative distribution function 
+# compared to a given empirical distribution function
+par(mfrow = c(2,2))
+plot(cv1); plot(cv0)
+plot(log(cv1$lambda), cv1$cvm , pch = 19, col = "red",
+     xlab = "log(Lambda)", ylab = cv1$name)
+points(log(cv0$lambda) , cv0$cvm , pch = 19, col = "blue")
+legend("topleft", legend = c("alpha = 1", "alpha = 0"),
+       pch = 19, col = c("red","blue"))
+
+# (4) Prediction
+# fitted values for the first 10 observations at 𝜆=min
+# lambda.min = lambda.min is the value of 𝜆 that gives minimum mean cross-validated error
+predict(cv1, as.matrix(x[1:10,]), s = "lambda.min")
+# lambda.1se is the value of 𝜆 that gives the most regularized model 
+# such that the cross-validated error is within one standard error of the minimum.
+predict(cv1, as.matrix(x[1:10,]), s = "lambda.1se")
