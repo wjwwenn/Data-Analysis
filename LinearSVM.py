7a40642d7f35bfb54fe23636900ca80f95325828
Jingwen <69618729+wjwwen@users.noreply.github.com>
2021-12-20 21:49:07 +0800

    Add files via upload

---
+++ LinearSVM.py
@@ -0,0 +1,87 @@
+# SUPPORT VECTOR MACHINES (SVM)
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+# use seaborn plotting defaults
+import seaborn as sns; sns.set()
+
+from sklearn.datasets import make_blobs
+X, y = make_blobs(n_samples=50, centers=2,
+                  random_state=0, cluster_std=0.60)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn');
+
+# SVM: Maximizing the Margin
+xfit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+
+for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]:
+    yfit = m * xfit + b
+    plt.plot(xfit, yfit, '-k')
+    plt.fill_between(xfit, yfit - d, yfit + d, edgecolor='none',
+                     color='#AAAAAA', alpha=0.4)
+
+plt.xlim(-1, 3.5);
+
+# FITTING SVM
+from sklearn.svm import SVC # "Support vector classifier"
+model = SVC(kernel='linear', C=1E10)
+model.fit(X, y)
+
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    # create grid to evaluate model
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    
+    # plot decision boundary and margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+    
+    # plot support vectors
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=1, facecolors='none');
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(model);
+
+# SUPPORT VECTORS (black circles)
+model.support_vectors_
+
+def plot_svm(N=10, ax=None):
+    X, y = make_blobs(n_samples=200, centers=2,
+                      random_state=0, cluster_std=0.60)
+    X = X[:N]
+    y = y[:N]
+    model = SVC(kernel='linear', C=1E10)
+    model.fit(X, y)
+    
+    ax = ax or plt.gca()
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+    ax.set_xlim(-1, 4)
+    ax.set_ylim(-1, 6)
+    plot_svc_decision_function(model, ax)
+
+fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+fig.subplots_adjust(left=0.0625, right=0.95, wspace=0.1)
+for axi, N in zip(ax, [60, 120]):
+    plot_svm(N, axi)
+    axi.set_title('N = {0}'.format(N))
+    
+    
+from ipywidgets import interact, fixed
+interact(plot_svm, N=[10, 200], ax=fixed(None));
