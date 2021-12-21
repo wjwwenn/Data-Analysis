c1d5ce8238dd21d3ca7f4e0ad17fb3dcb26a85af
Jingwen <69618729+wjwwen@users.noreply.github.com>
2021-12-21 22:37:52 +0800

    Add files via upload

---
+++ SVM/NonLinear_SVM.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set() # use seaborn plotting defaults
+from sklearn.datasets import make_circles
+from sklearn.svm import SVC # "Support vector classifier"
+
+# Kernelized SVM, find dsuitable nonlinear decision boundary
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    # create grid to evaluate model
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    
+    # plot decision boundary and margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+    
+    # plot support vectors
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=1, facecolors='none');
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+# Linearly separable on a higher order (additional dimension)
+X, y = make_circles(100, factor=.1, noise=.1)
+clf = SVC(kernel='linear').fit(X, y)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(clf, plot_support=False);
+
+# Compute a radial basis function centered on the middle clump
+r = np.exp(-(X ** 2).sum(1))
+
+# Kernel transformation: based on a similarity relationship (or kernel)
+# between each pair of points
+# Using 'kernel trick' so that we do not use computationally intensive N as it grows large
+import mpl_toolkits
+from mpl_toolkits import mplot3d
+
+clf = SVC(kernel='rbf', C=1E6)
+clf.fit(X, y)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(clf)
+plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1],
+            s=300, lw=1, facecolors='none');
