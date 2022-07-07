c3e72bfd09b260a45a5c705c50337af9b89ca1f7
Jingwen <69618729+wjwwen@users.noreply.github.com>
2022-07-07 15:23:27 +0800

    Add files via upload

---
+++ PythonBasics/5 class_args.py
@@ -0,0 +1,19 @@
+# Exercise 1 ----------------------------
+# arg = Positional Argument
+# kwarg = Keyword Argument
+abc = "new string"
+def some_func(arg_1, arg_2, kwarg_1=None, kwarg_2=None):
+    print(arg_1, arg_2)
+    if kwarg_1 != None:
+        print(kwarg_1)
+    # return arg_1
+    
+some_func("abc", "car", kwarg_1="Not a big deal")
+email_address = "another@gmail.com"
+to_list = ['abc@gmail.com']
+from_list = ['another2@gmail.com', 'hello@teamcfe.com']
+
+def send_email(email, to_list=to_list, from_list=from_list):
+    pass
+
+send_email("hello@teamcfe.com", to_list=['abc@gmail.com'], from_list=['abc@gmail.com'])
